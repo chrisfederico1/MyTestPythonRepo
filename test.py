24740be311362e8eafd53db77011aaa3117fd07f
chrisfederico1 <34996684+chrisfederico1@users.noreply.github.com>
2019-12-20 12:54:48 -0600

    First Commit

---
+++ test.py
@@ -0,0 +1,19 @@
+print('hello world')
+
+person = ["Dave","Ben","Emily"]
+
+for x in person:
+    if x  ==  "Ben":
+        print(x)
+    else:
+        print("not Ben")
+
+
+
+
+
+def my_function():
+    print("Hello from a function")
+
+
+my_function()
